d7ea9a98d683742aa80de2cf5b32e75c212d0d00
vsawantvinay <vsawant.azure@gmail.com>
2026-05-26 00:40:16 +0530

    config: update backend bucket name for my account

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "zen-pharma-terraform-state-chandika-s"
+    bucket = "zen-pharma-terraform-state-vsawantvinay"
     key    = "envs/dev/terraform.tfstate"
     encrypt      = true
     use_lockfile = true   # S3 native locking
